bc9b585964e0d027aed7f505eb4ae37c4deb4168
connor worthen <connorw220@protonmail.com>
2019-10-28 07:36:53 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,2 +1,6 @@
 def meal_choice(veg1, veg2, protein*) #with optional parameter
-  
+  puts "What a nutritious meal!"
+  puts "A plate of #{protein} with #{veg1} and #{veg2}."
+end
+
+p "A plate of #{meat} with #{kale} and #{spinach}"
